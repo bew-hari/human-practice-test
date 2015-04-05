4a8c1551f36e6ccf2e544300614158061a77b035
Bew Harichanwong <bew.hari@gmail.com>
2015-04-05 01:29:02 -0500

    Finish upload via HTML

--- app/controllers/upload_controller.rb
+++ app/controllers/upload_controller.rb
@@ -4,5 +4,18 @@ class UploadController < ApplicationController
 	end
 
 	def create
+		uploaded_file = params[:file]
+		
+		respond_to do |format|
+			format.html { 
+				session[:file_content] = uploaded_file.read
+				redirect_to analyze_path 
+			}
+		end
+	end
+
+	def analyze
+		@content = session[:file_content]
+		session[:file_content] = nil
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   
   root 'upload#index'
 
+  get 'analyze' => 'upload#analyze'
   post 'upload' => 'upload#create'
+
 
 end
